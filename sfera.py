b213997e02b533aebbed6aedfec097754226a2e1
DanielSetola <danielsetola@gmail.com>
2018-11-15 12:51:52 +0000

    Ho ricalcolao il volume della sfera utilizzando una funzione

--- sfera.py
+++ sfera.py
@@ -1,6 +1,15 @@
 import random
 import math
+
+
+
+def CalcoloVolume(r):
+	V=4/3*math.pi*r**3
+	return V
+
+
+# main
 r=random.randrange(0,21)
 print("Raggio:", r)
-V=4/3*math.pi*r**3
+V=CalcoloVolume(r)
 print("Volume:", V)
